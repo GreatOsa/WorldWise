079b64ce43cf89aadaf0ce0a0476e511dca8f08f
Agwaze Great <agwazegreat@gmail.com>
2025-10-03 22:29:57 +0100

    Implemented Homepage component with introductory text and background image.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="*" element={<PageNotFound />} />
+        {/* <div>App</div>; */}
+      </Routes>{" "}
+    </BrowserRouter>
+  );
 }
