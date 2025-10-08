fe687b864dfd52268a7916358abb255cba7b0afa
Agwaze Great <agwazegreat@gmail.com>
2025-10-08 20:11:14 +0100

    configure app routing and navigation between pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,33 @@ import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import { useEffect, useState } from "react";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+
+const Base_URL = "http://localhost:8000";
 
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${Base_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There is an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -13,6 +38,17 @@ export default function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>Hou</p>} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+        </Route>
 
         <Route path="*" element={<PageNotFound />} />
         {/* <div>App</div>; */}
---
+++ src/components/PageNav.jsx
@@ -0,0 +1,27 @@
+import { Link, NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+import Logo from "./Logo";
+
+export default function PageNav() {
+  return (
+    <nav className={styles.nav}>
+      <Link to={"/"}>
+        <Logo />
+      </Link>
+      <ul>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+
+        <li>
+          <NavLink className={styles.ctaLink} to="/login">
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
