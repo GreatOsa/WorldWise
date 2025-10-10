85f685feb50df939fee390da7d108ce09a7d0c3f
Agwaze Great <agwazegreat@gmail.com>
2025-10-10 06:42:32 +0100

    update logic in card file

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 export default function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
   const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -14,7 +16,9 @@ export default function CityItem({ city }) {
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji} </span>
