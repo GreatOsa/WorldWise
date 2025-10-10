1bcaa83ea7a72871dc31a4ab2a1614ce806f456b
Agwaze Great <agwazegreat@gmail.com>
2025-10-10 05:58:02 +0100

    use useContext to avoid prop drilling and add custom hook

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,10 @@ import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-export default function CountryList({ cities, isLoading }) {
+export default function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
@@ -23,7 +25,7 @@ export default function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
